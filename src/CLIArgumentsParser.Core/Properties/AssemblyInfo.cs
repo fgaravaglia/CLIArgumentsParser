0b1c735a810b7b79d2552bb6119d88f691abf40b
fgaravaglia <francesco.garavaglia@gmail.com>
2019-01-16 22:44:49 +0100

    Increased assembly version

--- src/CLIArgumentsParser.Core/Properties/AssemblyInfo.cs
+++ src/CLIArgumentsParser.Core/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CLIArgumentsParser.Core")]
 [assembly: AssemblyDescription("Library to easily manage and parse CLI arguments")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Umbrella")]
+[assembly: AssemblyCompany("Innova Solutions")]
 [assembly: AssemblyProduct("CLIArgumentsParser.Core")]
 [assembly: AssemblyCopyright("Copyright Umbrella ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
 [assembly: InternalsVisibleTo("CLIArgumentsParser.Tests")]
